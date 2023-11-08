0430fb9651903ddb2c13dee5e7da53ac945fd38a
Henry Till <henrytill@gmail.com>
2023-11-08 12:56:41 -0800

    host: tweak

--- host/noematic.ml
+++ host/noematic.ml
@@ -1,21 +1,21 @@
-let buffer_size = 1024
+let input_buffer_length = 1024
 
-let read_length chan =
-  let buffer = Bytes.create 4 in
-  match input chan buffer 0 4 with
-  | 4 -> Some (Bytes.get_int32_ne buffer 0)
+let read_length ic =
+  let bytes = Bytes.create 4 in
+  match input ic bytes 0 4 with
+  | 4 -> Some (Bytes.get_int32_ne bytes 0)
   | _ -> None
 
-let read_message chan length =
+let read_message ic length =
   let length = Int32.to_int length in
   let message_buffer = Buffer.create length in
-  let read_buffer = Bytes.create buffer_size in
+  let input_buffer = Bytes.create input_buffer_length in
   let rec go rem_bytes =
     if rem_bytes > 0 then
-      let bytes_to_read = min buffer_size rem_bytes in
-      let bytes_read = input chan read_buffer 0 bytes_to_read in
+      let bytes_to_read = min input_buffer_length rem_bytes in
+      let bytes_read = input ic input_buffer 0 bytes_to_read in
       if bytes_read > 0 then (
-        Buffer.add_subbytes message_buffer read_buffer 0 bytes_read;
+        Buffer.add_subbytes message_buffer input_buffer 0 bytes_read;
         go (rem_bytes - bytes_read))
       else
         raise (Failure "Unexpected end of file")
@@ -26,14 +26,19 @@ let read_message chan length =
 let handle_message oc length message =
   output_string oc (Bytes.to_string message);
   flush oc;
-  let length_buf = Bytes.create 4 in
-  Bytes.set_int32_ne length_buf 0 length;
-  print_bytes (Bytes.cat length_buf message);
+  let length_bytes =
+    begin
+      let bytes = Bytes.create 4 in
+      Bytes.set_int32_ne bytes 0 length;
+      bytes
+    end
+  in
+  output_bytes stdout (Bytes.cat length_bytes message);
   flush stdout
 
-let main () =
-  let tmp = Filename.temp_file "noematic-" ".txt" in
-  let oc = open_out tmp in
+let () =
+  let file = Filename.temp_file "noematic-" ".txt" in
+  let oc = open_out file in
   try
     while true do
       match read_length stdin with
@@ -43,5 +48,3 @@ let main () =
           handle_message oc length message
     done
   with End_of_file -> close_out_noerr oc
-
-let () = main ()
