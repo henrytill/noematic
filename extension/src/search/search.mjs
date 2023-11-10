355be19ca751b3a76725d92a106e5fe385ec0e6f
Henry Till <henrytill@gmail.com>
2023-11-10 01:41:39 -0800

    search: tweak

--- extension/src/search/search.mjs
+++ extension/src/search/search.mjs
@@ -1,24 +1,17 @@
-/**
- * @param {{query: string}} query
- */
-const sendQuery = async (query) => {
-  let message = { action: 'searchRequest', payload: { query: query } };
-  let response = await chrome.runtime.sendMessage(message);
-  console.log('response', response);
-};
-
 /** @type  {EventListener} */
 const listener = (event) => {
   event.preventDefault();
   const query = /** @type {HTMLInputElement?} */ (document.getElementById('search-input'));
-  if (!query) {
+  if (query == undefined) {
     throw new Error('No search input found');
   }
   const { value } = query;
   if (value.length === 0) {
     return;
   }
-  sendQuery({ query: value });
+  chrome.runtime
+    .sendMessage({ action: 'searchRequest', payload: { query: value } })
+    .then((response) => console.log('response', response));
 };
 
 /**
