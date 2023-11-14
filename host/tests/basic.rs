9ec95edf540cb1a5aaa029a2fbd336e9a3cf79f9
Henry Till <henrytill@gmail.com>
2023-11-13 23:21:39 -0800

    host: add failing test with hyphenated query

--- host/tests/basic.rs
+++ host/tests/basic.rs
@@ -182,3 +182,70 @@ fn test_search() {
 
     assert_eq!(expected, actual);
 }
+
+#[test]
+fn test_search_quotation() {
+    let noematic = exe("noematic").unwrap();
+    let mut child = Command::new(noematic)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to start child process");
+
+    let save_request = json!({
+        "version": "0.1.0",
+        "action": "saveRequest",
+        "payload": {
+            "url": "https://en.wikipedia.org/wiki/Foobar",
+            "title": "Title",
+            "innerText": "foo-bar-baz-quux"
+        },
+        "correlationId": "218ecc9f-a91a-4b55-8b50-2b6672daa9a5"
+    });
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    write_request(stdin, &save_request).expect("Failed to write request");
+
+    let expected = json!({
+        "version": "0.1.0",
+        "action": "saveResponse",
+        "payload": {},
+        "correlationId": "218ecc9f-a91a-4b55-8b50-2b6672daa9a5"
+    });
+
+    let stdout = child.stdout.as_mut().expect("Failed to open stdout");
+    let actual = read_response(stdout).expect("Failed to read response");
+
+    assert_eq!(expected, actual);
+
+    let search_request = json!({
+        "version": "0.1.0",
+        "action": "searchRequest",
+        "payload": {
+            "query": "foo-bar"
+        },
+        "correlationId": "218ecc9f-a91a-4b55-8b50-2b6672daa9a5"
+    });
+
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    write_request(stdin, &search_request).expect("Failed to write request");
+
+    let expected = json!({
+        "version": "0.1.0",
+        "action": "searchResponse",
+        "payload": {
+            "results": [
+                {
+                    "url": "https://en.wikipedia.org/wiki/Foobar",
+                    "title": "Title",
+                    "innerText": "foo-bar-baz-quux",
+                }
+            ]
+        },
+        "correlationId": "218ecc9f-a91a-4b55-8b50-2b6672daa9a5"
+    });
+
+    let stdout = child.stdout.as_mut().expect("Failed to open stdout");
+    let actual = read_response(stdout).expect("Failed to read response");
+
+    assert_eq!(expected, actual);
+}
