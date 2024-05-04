70857b936c5e100ecf042db5fb9aa06e8744c236
Henry Till <henrytill@gmail.com>
2024-05-03 17:36:38 -0700

    xtask: clean up

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -29,13 +29,19 @@ mod task {
 
     pub type Error = Box<dyn std::error::Error>;
 
-    const EXTENSION_ROOT: &str = "extension";
     const CARGO: &str = "cargo";
     const NPM: &str = "npm";
+
     const MSG_NO_PROJECT_ROOT: &str = "no project root";
 
+    macro_rules! npm_args {
+        ($command:expr) => {
+            ["--prefix", "extension", "run", $command]
+        };
+    }
+
     fn project_root() -> Option<PathBuf> {
-        Path::new(&env!("CARGO_MANIFEST_DIR")).ancestors().nth(1).map(Path::to_path_buf)
+        Path::new(env!("CARGO_MANIFEST_DIR")).ancestors().nth(1).map(Path::to_path_buf)
     }
 
     pub fn build() -> Result<(), Error> {
@@ -48,17 +54,13 @@ mod task {
         if !cargo_status.success() {
             Err("cargo build failed")?;
         }
-        let npm_status = Command::new(NPM)
-            .current_dir(&project_root)
-            .args(["--prefix", EXTENSION_ROOT, "run", "check"])
-            .status()?;
+        let npm_status =
+            Command::new(NPM).current_dir(&project_root).args(npm_args!("check")).status()?;
         if !npm_status.success() {
-            Err("npm run build failed")?;
+            Err("npm run check failed")?;
         }
-        let npm_status = Command::new(NPM)
-            .current_dir(&project_root)
-            .args(["--prefix", EXTENSION_ROOT, "run", "build"])
-            .status()?;
+        let npm_status =
+            Command::new(NPM).current_dir(&project_root).args(npm_args!("build")).status()?;
         if !npm_status.success() {
             Err("npm run build failed")?;
         }
@@ -71,14 +73,12 @@ mod task {
         let cargo_status =
             Command::new(cargo).current_dir(&project_root).args(["test"]).status()?;
         if !cargo_status.success() {
-            Err("cargo build failed")?;
+            Err("cargo test failed")?;
         }
-        let npm_status = Command::new(NPM)
-            .current_dir(&project_root)
-            .args(["--prefix", EXTENSION_ROOT, "run", "test"])
-            .status()?;
+        let npm_status =
+            Command::new(NPM).current_dir(&project_root).args(npm_args!("test")).status()?;
         if !npm_status.success() {
-            Err("npm run build failed")?;
+            Err("npm run test failed")?;
         }
         Ok(())
     }
@@ -87,7 +87,7 @@ mod task {
         let project_root = project_root().ok_or(MSG_NO_PROJECT_ROOT)?;
         let npm_status = Command::new(NPM)
             .current_dir(&project_root)
-            .args(["--prefix", EXTENSION_ROOT, "run", "create-host-manifest"])
+            .args(npm_args!("create-host-manifest"))
             .status()?;
         if !npm_status.success() {
             Err("npm run create-host-manifest failed")?;
