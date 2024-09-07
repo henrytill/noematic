58cb27a88beaa288084b477b58ca2ad0699a7bd5
Henry Till <henrytill@gmail.com>
2024-09-06 21:41:48 -0700

    extension: strip down eslint config

--- extension/eslint.config.mjs
+++ extension/eslint.config.mjs
@@ -1,34 +1,20 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 import globals from 'globals';
 
-export default tseslint.config(
-  { ignores: ['dist/**/*', '.prettierrc.cjs', 'eslint.config.mjs'] },
-  eslint.configs.recommended,
+export default [
+  js.configs.recommended,
   {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
+    ignores: ['.prettierrc.cjs', 'dist/**/*'],
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.webextensions,
-        ...globals.es2022,
       },
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-      ecmaVersion: 6,
     },
     rules: {
       'no-unused-vars': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
-);
+];
