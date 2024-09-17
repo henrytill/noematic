83718fc5f9c9dc2b971e6887013e8e224b18a364
Henry Till <henrytill@gmail.com>
2024-09-16 18:18:43 -0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.opam-repository.follows = "opam-repository";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      follows = "opam-nix/flake-utils";
+    };
+  };
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      package = "noematic";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope = on.buildOpamProject { resolveArgs.with-test = true; } package ./. {
+          ocaml-base-compiler = "5.2.0";
+        };
+        overlay = final: prev: { };
+      in
+      {
+        legacyPackages = scope.overrideScope' overlay;
+        packages.default = self.legacyPackages.${system}.${package};
+      }
+    );
+}
