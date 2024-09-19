c8dc736f6addc966400b6ca44040f3a556bec545
Henry Till <henrytill@gmail.com>
2024-09-18 23:05:30 -0700

    host: handle subcommands

--- host/bin/main.ml
+++ host/bin/main.ml
@@ -1,5 +1,20 @@
 open Noematic
 
+let exec_subcommand subcommand args =
+  let directory = Filename.dirname Sys.executable_name in
+  let executable = Printf.sprintf "noematic-%s" subcommand in
+  let executable_path = Filename.concat directory executable in
+  if Sys.file_exists executable_path then
+    Unix.execv executable_path (Array.of_list (executable :: args))
+  else (
+    Printf.eprintf "Error: %s not found in %s\n" executable directory;
+    exit 1)
+
+let handle_subcommand argv =
+  match Array.to_list argv with
+  | _ :: ("configure" as subcommand) :: args -> exec_subcommand subcommand args
+  | _ -> ()
+
 module Args = struct
   type t = { test : bool }
 
@@ -55,6 +70,7 @@ let rec process_messages context ic oc =
   process_messages context ic oc
 
 let () =
+  handle_subcommand Sys.argv;
   let args = Args.of_argv Sys.argv in
   let context = context_of_args args in
   let in_channel = stdin in
