d009230be9b4ac2c641cff5700f5658b85f7e5f7
Henry Till <henrytill@gmail.com>
2024-09-26 10:27:54 -0700

    host: Unix_ext: add signature, include Unix

--- host/lib/unix_ext.ml
+++ host/lib/unix_ext.ml
@@ -1,3 +1,5 @@
+include Unix
+
 let rec mkdir_all path perms =
   if path = "." || path = "/" then
     ()
@@ -6,6 +8,6 @@ let rec mkdir_all path perms =
     if not (Sys.file_exists parent) then
       mkdir_all parent perms;
     if not (Sys.file_exists path) then
-      try Unix.mkdir path perms with Unix.Unix_error (Unix.EEXIST, _, _) -> ()
+      try mkdir path perms with Unix_error (EEXIST, _, _) -> ()
     else if not (Sys.is_directory path) then
       failwith (Printf.sprintf "Error: %s exists but is not a directory" path)
---
+++ host/lib/unix_ext.mli
@@ -0,0 +1,3 @@
+include module type of Unix
+
+val mkdir_all : string -> file_perm -> unit
