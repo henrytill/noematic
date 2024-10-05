41cc0b8c2642f53316f89878c43e1654ec300d3e
Henry Till <henrytill@gmail.com>
2024-10-05 15:54:20 -0700

    host: Unix_ext: tweak caml_uname

--- host/lib/unix_ext.c
+++ host/lib/unix_ext.c
@@ -6,20 +6,17 @@
 
 CAMLprim value caml_uname(value unit) {
   CAMLparam1(unit);
-  CAMLlocal1(result);
   struct utsname buf;
-
-  if (uname(&buf) == -1) {
+  const int rc = uname(&buf);
+  if (rc == -1) {
     caml_failwith("uname failed");
   }
-
-  result = caml_alloc(5, 0);
-  Store_field(result, 0, caml_copy_string(buf.sysname));
-  Store_field(result, 1, caml_copy_string(buf.nodename));
-  Store_field(result, 2, caml_copy_string(buf.release));
-  Store_field(result, 3, caml_copy_string(buf.version));
-  Store_field(result, 4, caml_copy_string(buf.machine));
-
-  CAMLreturn(result);
+  CAMLlocal1(caml_result);
+  caml_result = caml_alloc(5, 0);
+  Store_field(caml_result, 0, caml_copy_string(buf.sysname));
+  Store_field(caml_result, 1, caml_copy_string(buf.nodename));
+  Store_field(caml_result, 2, caml_copy_string(buf.release));
+  Store_field(caml_result, 3, caml_copy_string(buf.version));
+  Store_field(caml_result, 4, caml_copy_string(buf.machine));
+  CAMLreturn(caml_result);
 }
-
