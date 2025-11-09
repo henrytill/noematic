c8cc8c9b929280324b435b0b5b019ab9ef7d6405
Henry Till <henrytill@gmail.com>
2025-11-09 14:31:22 -0800

    host: reformat create.sql

--- host/src/create.sql
+++ host/src/create.sql
@@ -17,21 +17,22 @@ CREATE TABLE IF NOT EXISTS sites (
 CREATE VIRTUAL TABLE IF NOT EXISTS sites_fts USING fts5(url, title, inner_text);
 
 CREATE TRIGGER sites_ai AFTER INSERT ON sites
-BEGIN
-    INSERT INTO sites_fts (rowid, url, title, inner_text)
-    VALUES (new.id, new.url, new.title, new.inner_text);
-END;
+    BEGIN
+        INSERT INTO sites_fts (rowid, url, title, inner_text)
+        VALUES (new.id, new.url, new.title, new.inner_text);
+    END;
 
 CREATE TRIGGER sites_au AFTER UPDATE ON sites
-BEGIN
-    UPDATE sites_fts SET
-        url = new.url,
-        title = new.title,
-        inner_text = new.inner_text
-    WHERE rowid = old.id;
-END;
+    BEGIN
+        UPDATE sites_fts
+           SET url = new.url,
+               title = new.title,
+               inner_text = new.inner_text
+         WHERE rowid = old.id;
+    END;
 
 CREATE TRIGGER sites_ad AFTER DELETE ON sites
-BEGIN
-    DELETE FROM sites_fts WHERE rowid = old.id;
-END;
+    BEGIN
+        DELETE FROM sites_fts
+         WHERE rowid = old.id;
+    END;
